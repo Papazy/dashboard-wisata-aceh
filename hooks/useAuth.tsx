97ca70af1d71796e346c2d58c0844ee81cb67031
Papazy <fajryjobs@gmail.com>
2025-01-20 04:09:51 +0700

    fix localStorage to not rendered in server side

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -12,73 +12,60 @@ const AuthContext = createContext<ProviderProps>({
   isAuthenticated: () => false,
 });
 
-
 // provider
-export const AuthProvider: React.FC<{children : React.ReactNode}> = ({children}) => {
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const isBrowser = typeof window !== "undefined"; // Periksa apakah window tersedia
 
   // set variable awal
-  const storedUser = localStorage.getItem('user');
-  const storedToken = localStorage.getItem('token');
+  const storedUser = isBrowser ? localStorage.getItem("user") : null;
+  const storedToken = isBrowser ? localStorage.getItem("token") : null;
   const [user, setUser] = useState<userType | null>(storedUser ? JSON.parse(storedUser) : null);
   const [token, setToken] = useState<string>(storedToken || "");
 
   console.log("Masuk ke useAuth");
 
   const router = useRouter();
-
-  // Mnedapatkan nilai awal dari local storage
-  // React.useEffect(() => {
-  //   const storedUser = localStorage.getItem("user");
-  //   const storedToken = localStorage.getItem("token");
-  
-  //   if (storedUser && storedToken) {
-  //     console.log('ada user di local storage')
-  //     setUser(JSON.parse(storedUser));
-  //     setToken(storedToken);
-  //   } else {
-  //   }
-  //   setIsLoading(false);
-  //   console.log("selesai periksa useAuth")
-  // }, []);
-
-
   const isAuthenticated = () => {
-    if(user){
+    if (user) {
       return true;
     }
     return token !== "";
-  }
+  };
 
   const getCurrentUser = async (thisToken: string) => {
     const useToken = thisToken || token;
-    try{
-      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/users/auth/me?secret_key=${process.env.NEXT_PUBLIC_BACKEND_API_KEY}`, {
-        headers: {
-          'Authorization': `Bearer ${useToken}`
-        },
-      })
-      if(res.ok){
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/users/auth/me?secret_key=${process.env.NEXT_PUBLIC_BACKEND_API_KEY}`,
+        {
+          headers: {
+            Authorization: `Bearer ${useToken}`,
+          },
+        }
+      );
+      if (res.ok) {
         const data = await res.json();
         setUser(data);
-        localStorage.setItem("user", JSON.stringify(data));
-        console.log("user data ", data)
-        return data      
-      }else{
+        if (isBrowser) {
+          localStorage.setItem("user", JSON.stringify(data));
+        }
+        console.log("user data ", data);
+        return data;
+      } else {
         return null;
       }
-    }catch(e : any){
-      console.log("Error getCurrentUser", e.message)
-      throw new Error("Failed to get user data")
+    } catch (e: any) {
+      console.log("Error getCurrentUser", e.message);
+      throw new Error("Failed to get user data");
     }
-  }
-
+  };
 
   // login untuk mendapatkan access
   const login = async (data: LoginProps) => {
     try {
       console.log("Masuk ke fungsi auth:", data);
       const { email, password } = data;
-  
+
       const response = await fetch(
         `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/users/auth/login?secret_key=${process.env.NEXT_PUBLIC_BACKEND_API_KEY}`,
         {
@@ -87,12 +74,13 @@ export const AuthProvider: React.FC<{children : React.ReactNode}> = ({children})
           body: new URLSearchParams({ username: email, password }),
         }
       );
-  
+
       const resData = await response.json();
-  
+
       if (response.ok) {
-        
-        localStorage.setItem("token", resData.access_token);
+        if (isBrowser) {
+          localStorage.setItem("token", resData.access_token);
+        }
 
         await getCurrentUser(resData.access_token);
         setToken(resData.access_token);
@@ -104,30 +92,23 @@ export const AuthProvider: React.FC<{children : React.ReactNode}> = ({children})
       throw error; // Propagate the error to the caller
     }
   };
-  
+
   // logout dengan menghapus token
-  const logout =  async() => {
-    localStorage.removeItem('user');
-    localStorage.removeItem('token');
+  const logout = async () => {
+    if (isBrowser) {
+      localStorage.removeItem("user");
+      localStorage.removeItem("token");
+    }
     setUser(null);
     setToken("");
-  }
-
-  // mendapatkan user yang sedang login
-
-
-  // if(isLoading){
-  //   return <div className="w-full h-screen flex justify-center items-center "><p>Loading...</p></div>
-  // }
+  };
 
   return (
     <AuthContext.Provider value={{ user, token, login, logout, isAuthenticated }}>
       {children}
     </AuthContext.Provider>
-  ) 
-}
+  );
+};
 
 // penggunaan useAuth
 export const useAuth = () => useContext(AuthContext);
-
-
